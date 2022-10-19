4f6f377755595d9854f581d1fdab2e93ea1df4d2
Bruno Maruwka <339804@uwr.edu.pl>
2022-10-19 13:21:58 +0200

    3komit

--- druga/szoste.py
+++ druga/szoste.py
@@ -2,4 +2,6 @@ studentlist=["Kasia","Basia","Marek","Darek"]
 studentlist.append("Józek")
 studentlist.sort()
 print(studentlist)
-print()
+print(studentlist[3],studentlist[:2],studentlist[-2:])
+studentlist.remove("Basia")
+print(studentlist)
