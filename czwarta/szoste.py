248504d4ead7555788cd8adb87611ef108bef37b
Bruno Maruwka <339804@uwr.edu.pl>
2022-11-23 13:26:11 +0100

    9kom

---
+++ czwarta/szoste.py
@@ -0,0 +1,22 @@
+def rgb_hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360 # wartosc od 0 do 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v      # h [0, 360] s, v [0, 1]
+
+print(rgb_hsv(50, 10, 0))
+print(rgb_hsv(0, 20, 30))
