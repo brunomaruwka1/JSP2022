3a2f028b63f73cffbab15b35b3a4d3e39171cd93
Bruno Maruwka <339804@uwr.edu.pl>
2023-01-04 12:57:21 +0100

    12kom

---
+++ siodma/test.py
@@ -0,0 +1,14 @@
+klasa=[{"imie":"Bruno","nazwisko":"Maruwka"},{"imie":"Ed","nazwisko":"JO"}]
+plik=open("listauczniocw.txt","w")
+
+#n=int(input("Podaj liczbe uczniow:"))
+
+#klasa=[{"imie":input("Podaj imie: "),"nazwisko":input("Podaj nazwisko: ")} for x in range(0,n)]
+
+klasa=sorted(klasa, key=lambda x: x['nazwisko'])
+print(klasa)
+
+for i in klasa:    
+    plik.write((f"{klasa.index(i)+1:2d} {i['imie']} {i['nazwisko']}\n"))
+
+plik.close()
