18261b72a70988c5a7f4d26bdfbacefbfbfc6a16
Bruno Maruwka <339804@uwr.edu.pl>
2023-02-01 12:04:34 +0100

    17

--- dziesiata/zadanie3.py
+++ dziesiata/zadanie3.py
@@ -5,8 +5,7 @@ class podlisty:
     def __init__(self, lista):
         self.lista = lista
     def tworzPodlist(self):
-        kombinacje = list(combinations(self.lista,3))
-        wyniki=[list(i) for i in kombinacje if sum(i)==0]
+        wyniki=[k for k in list(combinations(self.lista,3)) if sum(k)==0]
         return(wyniki)
 
 test = podlisty([1, -3, 4, 1, 2, 0, 7, 2, 5, -2, -4, -1])
